173140d011755adfecd14704cfe586fff04c4275
SantiGonzalezz <s.gonzalez35@uniandes.edu.co>
2023-04-11 06:13:51 -0500

    correcion RFC2

--- RFC/RFC2.sql
+++ RFC/RFC2.sql
@@ -3,4 +3,6 @@ SELECT A_Habitacion.id, COUNT(A_Reserva.id) AS numreservas
 FROM A_Habitacion
 INNER JOIN A_Reserva ON A_Habitacion.id = A_Reserva.idHabitacion
 GROUP BY A_Habitacion.id
-ORDER BY COUNT(A_Reserva.id) DESC, A_Habitacion.id ASC;
+ORDER BY COUNT(A_Reserva.id) DESC, A_Habitacion.id ASC
+FETCH FIRST 20 ROWS ONLY
+;
