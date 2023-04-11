a814368d664bbaf44e4d1d92a518be5cc396354d
diegorubio100 <98600889+diegorubio100@users.noreply.github.com>
2023-04-11 13:10:32 -0500

    Add files via upload

---
+++ LimpiezaAlohandes.sql
@@ -0,0 +1,17 @@
+DROP TABLE A_HabitacionUniversitaria;
+DROP TABLE A_Vivienda;
+DROP TABLE A_ApartamentoComunidad;
+DROP TABLE A_HabitacionFenicia;
+DROP TABLE A_HabitacionHostal;
+DROP TABLE A_HabitacionHotel;
+DROP TABLE A_ResidenciaUniversitaria;
+DROP TABLE A_AlquilaDia;
+DROP TABLE A_AlquilaMes;
+DROP TABLE A_Fenicia;
+DROP TABLE A_Hostal;
+DROP TABLE A_Hotel;
+DROP TABLE A_Disponibilidad;
+DROP TABLE A_Reserva;
+DROP TABLE A_Habitacion;
+DROP TABLE A_VinculadoUniandes;
+commit;
