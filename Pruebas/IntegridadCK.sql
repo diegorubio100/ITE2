ca89583a0415047eef5d869bec5b879bd295478e
SantiGonzalezz <s.gonzalez35@uniandes.edu.co>
2023-04-11 19:20:45 -0500

    Terminado pruebas

--- Pruebas/IntegridadCK.sql
+++ Pruebas/IntegridadCK.sql
@@ -1,1 +1,5 @@
--- 
+-- VinculadoUniandes
+-- Tupla cumple
+insert into A_VinculadoUniandes (id, nombre, tipo) values (1, 'Krysta', 'Empleado');
+-- Tupla viola
+insert into A_VinculadoUniandes (id, nombre, tipo) values (2, 'Krysta', 'NoEsNada');
